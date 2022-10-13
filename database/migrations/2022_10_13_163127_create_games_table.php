7b163fd205895f06c9384ecf1fff3cd8d2aed35b
Moroz Ivan <VanekMRZ@yandex.ru>
2022-10-13 19:48:54 +0300

    Создание миграций для работы игр и турниров

---
+++ database/migrations/2022_10_13_163127_create_games_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_tournament');
+            $table->integer('id_team_1');
+            $table->integer('id_team_2');
+            $table->integer('count_team_1');
+            $table->integer('count_team_2');
+            $table->timestamp('date_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
