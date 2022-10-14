73ecb5f2197739da0af348c0956b2e2859a4edee
Moroz Ivan <VanekMRZ@yandex.ru>
2022-10-15 02:33:07 +0300

    Произведен рефакторинг создания/изменения команды

--- app/Http/Livewire/AddTeam.php
+++ app/Http/Livewire/AddTeam.php
@@ -12,48 +12,79 @@ use Livewire\Component;
 
 class AddTeam extends Component
 {
-    public $current_team = 0;
+    //    Переменная открытия-закрытия формы
     public $modalFormVisible = false;
-    public $name, $userForm="";
-    public $idUserForm0 = 0, $idUserForm1 = 0, $idUserForm2 = 0, $idUserForm3 = 0, $idUserForm4 = 0;
-    public $idSelectedRobot0 = 0, $idSelectedRobot1 = 0, $idSelectedRobot2 = 0, $idSelectedRobot3 = 0, $idSelectedRobot4 = 0;
-    public $counterUserForms = 0, $maxNumForms = 5;
+    //    Переменные формы
+    public $name, $selected_users_id = [];
+    //    Переменные отображения
+    public $users = [], $MAX_SELECTED_USERS = 5;
+
+    public $current_team = 0;
+
     public $errorOutput;
 
     protected $rules = [
        'name' => 'required|min:2',
        // 'name' => 'required|null'
         ];
-
-    public function editShowModal(){
-        $team = Team::where("id", $this->current_team)->get()->first();
-        $players = Player::where("id_team", $team->id)->get();
-        $this->name = $team->name;
-        $this->counterUserForms = count($players);
-        for ($i = 0; $i < $this->counterUserForms; $i++) {
-            $this->{"idUserForm".$i} = $players[$i]->id_user;
-            $this->{"idSelectedRobot".$i} = $players[$i]->id_robot;
-        }
-        $this->userForm = $this->getUserForm();
+    public function createShowModal(){
+        $this->users = User::all();
         $this->modalFormVisible = true;
+    }
+    public function editShowModal(){
+        $this->users = User::all();
+        $team = Team::where("id", $this->current_team)->get()->first();
+        $this->selected_users_id = Player::where("id_team", $team->id)->pluck('id_user');
+        $this->name = $team->name;
+        $this->modalFormVisible = true;
+    }
+    public function addUser(){
+        array_push($this->selected_users_id, 0);
+    }
+    public function removeUser($index){
+        unset($this->selected_users_id[$index]);
+    }
+
+    public function adding(){
+        $this->validate();
+        if(true/*$this->validateForms()*/) {
+            $id_team = DB::table('teams')->insertGetId([
+                'name' => $this->name,
+                'created_at' => date("Y-m-d H:i:s", strtotime('now')),
+                'updated_at' => date("Y-m-d H:i:s", strtotime('now')),
+            ]);
+            foreach($this->selected_users_id as $users_id) {
+                DB::table('players')->insert([
+                    'id_team' => $id_team,
+                    'id_user' => $users_id,
+                    'created_at' => date("Y-m-d H:i:s", strtotime('now')),
+                    'updated_at' => date("Y-m-d H:i:s", strtotime('now')),
+                ]);
+            }
+            $this->modalFormVisible = false;
+            redirect("/teams", [\App\Http\Controllers\Teams\TeamsController::class, 'index']);
+        }
+        else {
+            $this->errorOutput .= 'Сделайте окончательный выбор игроков и роботов!';
+        }
     }
     public function modification(){
         $this->validate();
-        if($this->validateForms()) {
+        if(true/*$this->validateForms()*/) {
             Team::where('id', $this->current_team)->update([
                 'name' => $this->name,
                 'updated_at' => date("Y-m-d H:i:s", strtotime('now')),
             ]);
             Player::where("id_team", $this->current_team)->delete();
-            for ($i = 0; $i < $this->counterUserForms; $i++) {
+            foreach ($this->selected_users_id as $users_id){
                 DB::table('players')->insert([
                     'id_team' => $this->current_team,
-                    'id_user' => $this->{"idUserForm" . $i},
-                    'id_robot' => $this->{"idSelectedRobot" . $i},
+                    'id_user' => $users_id,
                     'created_at' => date("Y-m-d H:i:s", strtotime('now')),
                     'updated_at' => date("Y-m-d H:i:s", strtotime('now')),
                 ]);
             }
+
             $this->modalFormVisible = false;
             redirect("/teams", [\App\Http\Controllers\Teams\TeamsController::class, 'index']);
         }
@@ -62,92 +93,12 @@ class AddTeam extends Component
         }
     }
 
-
-
-    public function createUserForm(){
-        $this->counterUserForms++;
-        $this->userForm = $this->getUserForm();
-    }
-    public function removeUserForm(){
-        $this->counterUserForms--;
-        ${"idUserForm".$this->counterUserForms} = 0; ${"idSelectedRobot".$this->counterUserForms} = 0;
-
-        $this->userForm = $this->getUserForm();
-    }
-
-    public function refreshUserForm(){
-        $this->userForm = $this->getUserForm();
-    }
-    private function getUserForm(): string
-    {
-        $result = "";
-        for ($i=0; $i < $this->counterUserForms; $i++){
-            $result .= '<select wire:model="idUserForm'.$i.'" wire:click="refreshUserForm">'.$this->getListUsers().'</select>';
-            $currentIdUserForm = $this->{"idUserForm".$i};
-            if ($currentIdUserForm != 0) {
-                $roboList = Robot::where('id_master', $currentIdUserForm)->where('is_working', 1)->get();
-                if (count($roboList) > 0) {
-                    $robotsOptions = "";
-                    foreach($roboList as $robot){
-
-                        $robotsOptions .= '<option value="'.$robot->id.'">'.$robot->name.'</option>';
-                    }
-                    $result .= '<select wire:model="idSelectedRobot'.$i.'">'.$robotsOptions.'</select>';
-                    $this->{"idSelectedRobot".$i} = $roboList[0]->id;
-                } else {
-                    $result .= "У этого пользователя нет роботов!";
-                }
-            }
-            $result .= "<br>";
-        }
-
-        return $result;
-    }
-    private function getListUsers(): string
-    {
-        $result = [];
-        $listUsers = User::all();
-        foreach($listUsers as $user){
-            $elem = '<option value="'.$user->id.'" >'.$user->name.'</option>';
-            array_push($result, $elem);
-        }
-        array_unshift(   $result, '<option selected>Выберите игрока</option>');
-        return implode($result);
-    }
-    public function createShowModal(){
-        $this->modalFormVisible = true;
-    }
-    public function adding(){
-        $this->validate();
-        if($this->validateForms()) {
-            $id_team = DB::table('teams')->insertGetId([
-                'name' => $this->name,
-                'id_place' => /*$this->id_place*/0,
-                'created_at' => date("Y-m-d H:i:s", strtotime('now')),
-                'updated_at' => date("Y-m-d H:i:s", strtotime('now')),
-            ]);
-            for ($i = 0; $i < $this->counterUserForms; $i++) {
-                DB::table('players')->insert([
-                    'id_team' => $id_team,
-                    'id_user' => $this->{"idUserForm" . $i},
-                    'id_robot' => $this->{"idSelectedRobot" . $i},
-                    'created_at' => date("Y-m-d H:i:s", strtotime('now')),
-                    'updated_at' => date("Y-m-d H:i:s", strtotime('now')),
-                ]);
-            }
-            $this->modalFormVisible = false;
-            redirect("/teams", [\App\Http\Controllers\Teams\TeamsController::class, 'index']);
-        }
-        else {
-            $this->errorOutput .= 'Сделайте окончательный выбор игроков и роботов!';
-        }
-    }
     public function render()
     {
         return view('livewire.add-team');
     }
 
-    private function validateForms(): bool
+/*    private function validateForms(): bool
     {
         if($this->counterUserForms == 0)
             return false;
@@ -159,9 +110,7 @@ class AddTeam extends Component
             }
         }
         return true;
-    }
-/*    public function remove(){
-        $modalFormVisible = false;
     }*/
+
 
 }
