607a6bfa3d36fde6e68ad77280c3fd73c7ed2c8d
IrinaMoroz <moroz.irina3373@gmail.com>
2022-10-28 21:52:28 +0300

    Внедрена страничка "Информация об игре"

--- app/Http/Controllers/Games/Game/GameController.php
+++ app/Http/Controllers/Games/Game/GameController.php
@@ -4,15 +4,50 @@ namespace App\Http\Controllers\Games\Game;
 
 use App\Http\Controllers\Controller;
 use App\Models\Game;
+use App\Models\Place;
+use App\Models\Player;
+use App\Models\Robot;
+use App\Models\Team;
+use App\Models\Tournament;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
     public function index($id_game){
-        $item = Game::find($id_game);
+        $game = Game::find($id_game);
+        $tournament = Tournament::find($game->id_tournament);
+        $organizer = User::find($tournament->id_creator);
+        $place = Place::find($tournament->id_place);
+        $information_team1 = Team::find($game->id_team_1);
+        $information_team2 = Team::find($game->id_team_2);
+        $users_team1 = Player::where('id_team', $game->id_team_1)->
+        leftJoin('users','players.id_user', '=','users.id')->get();
+        $users_team2 = Player::where('id_team', $game->id_team_2)->
+        leftJoin('users','players.id_user', '=','users.id')->get();
+        $users_robots1 = Player::where('id_team', $game->id_team_1)->get();
+
+        $list1Robots = [];
+        foreach ($users_team1 as $user){
+            array_push($list1Robots, Robot::where('id_master', $user->id_user)->get());
+        }
+
+        $list2Robots = [];
+        foreach ($users_team2 as $user){
+            array_push($list2Robots, Robot::where('id_master', $user->id_user)->get());
+        }
 
         return view('pages.game',[
-            'game' => $item
+            'game' => $game,
+            'team_1' => $information_team1,
+            'team_2' => $information_team2,
+            'users_team1' => $users_team1,
+            'users_team2' => $users_team2,
+            'list1Robots' => $list1Robots,
+            'list2Robots' => $list2Robots,
+            'tournament' => $tournament,
+            'organizer' => $organizer,
+            'place' => $place
         ]);
     }
 }
--- resources/views/content/games/game/game.blade.php
+++ resources/views/content/games/game/game.blade.php
@@ -1,1 +1,78 @@
-<br/><br/><br/><br/> {{$game}}
+<!-- Banner -->
+<section id="banner">
+    <div class="inner">
+        <h2>ИНФОРМАЦИЯ ОБ ИГРЕ<br>{{$team_1->name}} VS {{$team_2->name}}</h2>
+        <h4>{{$game->date_time}}</h4>
+        <!--<p>Another fine responsive<br />
+            site template freebie<br />
+            crafted by <a href="http://html5up.net">HTML5 UP</a>.</p>-->
+        <ul class="actions special">
+            <li><a href="#" class="button big-button">ВЕСТИ СЧЁТ</a></li>
+        </ul>
+    </div>
+</section>
+
+</div>
+<section>
+    <div class="block-icon">
+
+        <button class="btn"><i class="fa fa-edit" style="font-size:30px"></i></button>
+    </div>
+</section>
+<section>
+
+    <div class="block-game">
+
+        <div id="block1">
+            <div class="rectangle3">{{$game->count_team_1}}</div>
+            <div class="heading">{{$team_1->name}}<hr class="hr-line1"></div>
+            <div class="subtitle">Состав команды</div>
+            @foreach($users_team1 as $user)
+            <div class="rectangle1"><a href="#" class="link-name1">{{$user->name}}</a></div>
+            @endforeach
+            <div class="subtitle">Роботы</div>
+            @foreach($list1Robots as $robots)
+                @foreach($robots as $robot)
+                    <div class="rectangle1"><a href="#" class="link-name1"> {{($robot->name)}}</a></div>
+                @endforeach
+            @endforeach
+
+        </div>
+        <div id="block2">
+            <div class="rectangle4">{{$game->count_team_2}}</div>
+            <div class="heading">{{$team_2->name}}<hr class="hr-line2"></div>
+            <div class="subtitle">Состав команды</div>
+            @foreach($users_team2 as $user)
+                <div class="rectangle2"><a href="#" class="link-name2">{{$user->name}}</a></div>
+            @endforeach
+            <div class="subtitle">Роботы</div>
+            @foreach($list2Robots as $robots)
+                @foreach($robots as $robot)
+                    <div class="rectangle2"><a href="#" class="link-name2"> {{$robot->name}}</a></div>
+                @endforeach
+            @endforeach
+
+        </div>
+
+    </div>
+
+</section>
+<div class="block-second">
+    <div class="table">
+        <table>
+            <tr>
+                <td class="subtitle">Турнир</td>
+                <td align="right"><a href="/games" class="link-name1">{{$tournament->name}}</a></td>
+            </tr>
+            <tr>
+                <td class="subtitle">Площадка</td>
+                <td align="right"><a href="/places/{{$place->id}}" class="link-name1">{{$place->name}}</a></td>
+            </tr>
+            <tr>
+                <td class="subtitle">Организатор</td>
+                <td align="right"><a href="#" class="link-name1">{{$organizer->name}}</a></td>
+            </tr>
+
+        </table>
+    </div>
+</div>
