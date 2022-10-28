0c96e8aff8af2848e346e98a9581ef6dd5d91f17
Moroz Ivan <VanekMRZ@yandex.ru>
2022-10-29 02:36:48 +0300

    Реализована страница Мои турниры

--- resources/views/content/games/tournaments/tournaments.blade.php
+++ resources/views/content/games/tournaments/tournaments.blade.php
@@ -1,3 +1,83 @@
-@php
-    print_r($tournaments);
-@endphp
+<!-- Banner -->
+<section id="banner">
+    <div class="inner">
+        <h2>Мои Турниры</h2>
+        <p>Подтвердите участие в<br />
+            грядущих турнирах</p>
+    </div>
+    <a href="#one" class="more scrolly">Читать далее</a>
+</section>
+
+<section id="one" class="wrapper style5">
+    <div class="inner">
+        <div class="not-found">Турниров с вашим участием нет!</div>
+
+        <section class="tournament">
+            <h4>Название турнира</h4>
+            <h5>Название площадки</h5>
+            <h5>27.01.2022 12:23:00</h5>
+            <p>Ваша команда Старкит участвует в турнире Робоармия-2022, на площадке Парк Партиот, дата-время: 27.01.2022 12:00. Подтвердите свое участие.</p>
+
+            <div class="table-wrapper">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Участник</th>
+                        <th>Подтвердил участие</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Николай Еремин</td>
+                            <td><i class="fa fa-times"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Александр Потапов</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="footer-wrapper">
+                    <button class="button big-button">ПОДТВЕРДИТЬ УЧАСТИЕ</button>
+                </div>
+
+            </div>
+        </section>
+
+
+        <section class="tournament">
+            <h4>Название турнира</h4>
+            <h5>Название площадки</h5>
+            <h5>27.01.2022 12:23:00</h5>
+            <p>Ваша команда Старкит участвует в турнире Робоармия-2022, на площадке Парк Партиот, дата-время: 27.01.2022 12:00. Подтвердите свое участие.</p>
+
+            <div class="table-wrapper">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Участник</th>
+                        <th>Подтвердил участие</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Николай Еремин</td>
+                        <td><i class="fa fa-check"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Александр Потапов</td>
+                        <td><i class="fa fa-check"></i></td>
+                    </tr>
+                    </tbody>
+                </table>
+                <div class="footer-wrapper">
+                    УЧАСТИЕ ПОДТВЕРЖДЕНО!
+                </div>
+
+            </div>
+        </section>
+
+    </div>
+</section>
+
+
