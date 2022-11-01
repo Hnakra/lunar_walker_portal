5e174a82cbc078df4fc80ed8b0fc3efd778fe10e
Olga Antipovskaia <Olga240301@bk.ru>
2022-11-01 23:29:27 +0300

    Внедрена страница Пользователи

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\Player;
 use App\Models\Robot;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,6 +13,9 @@ class UsersController extends Controller
 {
     public function index(){
         $items = User::all();
+        foreach ($items as $user){
+            $user->teams = Player::where('id_user', $user->id)->leftJoin('teams', 'teams.id', '=', 'players.id_team')->get();
+        }
 
         return view('pages.users',[
             'users' => $items
--- resources/views/content/users/users.blade.php
+++ resources/views/content/users/users.blade.php
@@ -1,5 +1,30 @@
-<h1 style="padding-top: 100px">@yield('title')</h1>
-@foreach($users as $user)
-    <div>{{$user->name}}</div>
-@endforeach
-<br>
+<!-- Banner -->
+<section id="banner">
+    <div class="inner">
+        <h2>ПОЛЬЗОВАТЕЛИ</h2>
+        <!--<p>Another fine responsive<br />
+            site template freebie<br />
+            crafted by <a href="http://html5up.net">HTML5 UP</a>.</p>-->
+    </div>
+    <a href="#one" class="more scrolly">Читать далее</a>
+</section>
+
+{{--<h1 style="padding-top: 100px">@yield('title')</h1>--}}
+<section class="cards wrapper style5">
+    @foreach($users as $user)
+        <div class="card">
+            <div class="round-image" style='background-image: {{$user->user_photo_profile}}'></div>
+            <div><a href="/users/{{$user->id}}"><b>{{$user->name}}</b></a></div>
+            @if($user->teams->count() == 1)
+                <p><small>Команда "{{$user->teams->first()->name}}"</small></p>
+            @endif
+            @if($user->teams->count() >= 2)
+                <p><small>Команды
+                        @foreach($user->teams as $team)
+                            "{{$team->name}}"
+                        @endforeach
+                </small></p>
+            @endif
+        </div>
+    @endforeach
+</section>
