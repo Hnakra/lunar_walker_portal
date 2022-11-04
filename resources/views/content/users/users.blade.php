95574382ddfc34eedffe766b64c1ac561a5cd7a4
Moroz Ivan <VanekMRZ@yandex.ru>
2022-11-05 00:58:24 +0300

    Починил картинки списка пользователей

--- resources/views/content/users/users.blade.php
+++ resources/views/content/users/users.blade.php
@@ -13,7 +13,7 @@
 <section class="cards wrapper style5">
     @foreach($users as $user)
         <div class="card">
-            <div class="round-image" style='background-image: {{$user->user_photo_profile}}'></div>
+            <div class="round-image" style='background-image: url("../storage/{{$user->profile_photo_path}}")'></div>
             <div><a href="/users/{{$user->id}}"><b>{{$user->name}}</b></a></div>
             @if($user->teams->count() == 1)
                 <p><small>Команда "{{$user->teams->first()->name}}"</small></p>
