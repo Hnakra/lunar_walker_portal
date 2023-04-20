8d61cdf82363efea9ede138aa00039030977930d
Moroz Ivan <VanekMRZ@yandex.ru>
2023-04-20 22:54:28 +0300

    Простой реформат

--- tests/Feature/AvailabilityPagesTest.php
+++ tests/Feature/AvailabilityPagesTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\Game;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AvailabilityPagesTest extends TestCase
@@ -14,61 +12,74 @@ class AvailabilityPagesTest extends TestCase
     {
         $this->get('/')->assertStatus(200);
     }
+
     public function test_availability_page_users()
     {
         $this->get('/users')->assertStatus(200);
     }
+
     public function test_availability_page_robots()
     {
         $this->get('/robots')->assertStatus(200);
     }
+
     public function test_availability_page_places()
     {
         $this->get('/places')->assertStatus(200);
     }
+
     public function test_availability_page_teams()
     {
         $this->get('/teams')->assertStatus(200);
     }
+
     public function test_availability_page_games()
     {
         $this->get('/games')->assertStatus(200);
     }
+
     public function test_availability_page_about_us()
     {
         $this->get('/about_us')->assertStatus(200);
     }
+
     public function test_availability_page_tournaments()
     {
         $this->actingAs(User::first())->get('/tournaments')->assertStatus(200);
     }
+
     public function test_availability_page_statistic()
     {
         $this->get('/statistic')->assertStatus(200);
     }
+
     public function test_availability_page_login()
     {
         $this->get('/login')->assertStatus(200);
     }
+
     public function test_availability_page_register()
     {
         $this->get('/register')->assertStatus(200);
     }
+
     public function test_availability_page_profile()
     {
         $this->actingAs(User::first())->get('/user/profile')->assertStatus(200);
     }
+
     public function test_availability_page_game()
     {
         $game = Game::first();
-        if(isset($game)){
+        if (isset($game)) {
             $this->get("/game/$game->id")->assertStatus(200);
         }
     }
+
     public function test_availability_game_counter()
     {
         $game = Game::first();
-        if(isset($game)){
+        if (isset($game)) {
             $this->actingAs(User::first())->get("/game/$game->id/counter")->assertStatus(200);
         }
     }
