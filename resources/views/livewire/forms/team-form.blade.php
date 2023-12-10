06b1c2f0ee3411e34fc32a32ca93c4a6ac9d5f38
Moroz Ivan <vanekmrz@yandex.ru>
2023-12-10 16:49:29 +0300

    Сделано так, что в качестве тренера может быть выбран другой тренер

--- resources/views/livewire/forms/team-form.blade.php
+++ resources/views/livewire/forms/team-form.blade.php
@@ -31,7 +31,7 @@
             <span>{{__('Тренер')}}</span>
             <select class="child-form" wire:model="selected_trainer">
                 <option value ="0">{{__('Нет тренера')}}</option>
-                @foreach($users as $user)
+                @foreach(User::where('id_role', 3)->get() as $user)
                     <option value="{{$user->id}}">{{$user->name}}</option>
                 @endforeach
             </select>
